581ff7fe25198d68433e783381309d4330d215c8
timbalistru1990 <anisoara4>
2018-06-27 18:02:09 +0300

    class DVD PLAYER

---
+++ src/Chapter2/DVDPlayer.java
@@ -0,0 +1,14 @@
+package Chapter2;
+
+public class DVDPlayer {
+
+    boolean canRecord = false;
+
+    void playDVD() {
+        System.out.println(" The DVD is played  ");
+    }
+
+    void recordDVD() {
+        System.out.println(" Is DVD recording  ");
+    }
+}
