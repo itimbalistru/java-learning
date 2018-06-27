b0e3de6e598182bce588ef3dba3e6d32b03d6ceb
timbalistru1990 <anisoara4>
2018-06-27 10:23:41 +0300

    chapter 2, page 72

---
+++ src/Chapter2/TapeDeck.java
@@ -0,0 +1,15 @@
+package Chapter2;
+
+public class TapeDeck {
+
+    boolean canRecord = false;
+
+    void playTape() {
+        System.out.println(" The pellicle is played ");
+    }
+
+    void recordTape() {
+        System.out.println(" Is recording on pellicle ");
+    }
+}
+
---
+++ src/Chapter2/TapeDeckTestDrive.java
@@ -0,0 +1,14 @@
+package Chapter2;
+
+class TapeDeckTestDrive {
+    public static void main(String[] args) {
+
+        TapeDeck t = new TapeDeck();
+        t.canRecord = true;
+        t.playTape();
+
+        if (t.canRecord == true) {
+            t.recordTape();
+        }
+    }
+}
