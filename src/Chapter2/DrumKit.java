7b4b0ebdf79dcd8cb7173b188ec4558aec4d8c6a
timbalistru1990 <anisoara4>
2018-06-28 14:34:23 +0300

    class DrumKit

---
+++ src/Chapter2/DrumKit.java
@@ -0,0 +1,16 @@
+package Chapter2;
+
+public class DrumKit {
+
+    boolean topHat = true;
+    boolean snare = true;
+
+    void playSnare () {
+        System.out.println("bah bah ba-bah");
+    }
+
+    void playTopHat () {
+        System.out.println("zing zing zi-zing");
+    }
+
+}
